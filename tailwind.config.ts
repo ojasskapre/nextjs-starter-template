6fc196043c059de0751bc2daac3ba6de10873059
ojasskapre <ojasskapre@gmail.com>
2024-08-17 10:37:23 -0700

    init next.js project setup

---
+++ tailwind.config.ts
@@ -0,0 +1,48 @@
+import type { Config } from 'tailwindcss';
+
+const config: Config = {
+  content: [
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        foreground: 'var(--foreground)',
+        background: 'var(--background)',
+        primary: 'var(--primary)',
+        secondary: 'var(--secondary)',
+        card: 'var(--card)',
+        'card-foreground': 'var(--card-foreground)',
+        'card-hover': 'var(--card-hover)',
+        popover: 'var(--popover)',
+        'popover-foreground': 'var(--popover-foreground)',
+        muted: 'var(--muted)',
+        'muted-foreground': 'var(--muted-foreground)',
+        accent: 'var(--accent)',
+        'accent-foreground': 'var(--accent-foreground)',
+        destructive: 'var(--destructive)',
+        'destructive-foreground': 'var(--destructive-foreground)',
+        border: 'var(--border)',
+        input: 'var(--input)',
+        ring: 'var(--ring)',
+        success: 'var(--success)',
+        'success-foreground': 'var(--success-foreground)',
+        error: 'var(--error)',
+        'error-foreground': 'var(--error-foreground)',
+        warning: 'var(--warning)',
+        'warning-foreground': 'var(--warning-foreground)',
+        info: 'var(--info)',
+        'info-foreground': 'var(--info-foreground)',
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
